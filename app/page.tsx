d19e963718ca0c82528d7c818638a1828b894881
axis1410 <1410axis@gmail.com>
2024-02-02 17:44:44 +0530

    Add loading indicator and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,30 @@
 "use client";
 
 import LocationInput from "@/components/LocationInput";
-import OtherData from "@/components/OtherData";
 import { WeatherCard } from "@/components/WeatherCard";
 import { airQualityLevel } from "@/utils/airQualityLevel";
 import { defaultWeather } from "@/utils/defaultWeather";
 import { parseRequestBody } from "@/utils/parseRequestBody";
 import axios, { AxiosError } from "axios";
 import { FormEvent, useState } from "react";
+import { Commet } from "react-loading-indicators";
 
 export default function Home() {
   const [weatherData, setWeatherData] = useState<Weather>(defaultWeather);
-  const [location, setLocation] = useState("");
-  const [isCelcius, setIsCelcius] = useState(true);
-  const [currentTime, setCurrentTime] = useState("");
-  const [errorMessage, setErrorMessage] = useState("Waiting for input");
-  const [airQualityText, setAirQualityText] = useState("");
+  const [location, setLocation] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isCelcius, setIsCelcius] = useState<boolean>(true);
+  const [currentTime, setCurrentTime] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string | null>("Waiting for input");
+  const [airQualityText, setAirQualityText] = useState<string>("");
 
   const apiKey = process.env.WEATHER_API_KEY;
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    setIsLoading(true);
+    setErrorMessage(null);
 
     const requestLocation = parseRequestBody(location);
 
@@ -42,6 +46,9 @@ export default function Home() {
           // @ts-ignore
           setErrorMessage(err.response?.data.error.message);
         }
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
 
@@ -67,15 +74,17 @@ export default function Home() {
             isCelcius={isCelcius}
             currentTime={currentTime}
           />
-          <OtherData airQualityText={airQualityText} />
         </div>
       ) : (
-        <div className={`text-5xl text-center text-white`}>{errorMessage}</div>
+        <div className={`text-5xl text-center text-white`}>
+          {isLoading ? <Commet color="#ffffff" size="medium" text="" textColor="" /> : errorMessage}
+        </div>
       )}
+
       <button
         type="button"
         className="fixed bottom-0 right-0 sm:h-20 sm:w-20 h-10 w-10 rounded-full sm:m-10 m-4 
-        bg-black hover:bg-gray-900 text-white text-lg sm:text-3xl"
+          bg-black hover:bg-gray-900 text-white text-lg sm:text-3xl"
         onClick={handleChangeUnits}
       >
         {isCelcius ? "C" : "F"}
